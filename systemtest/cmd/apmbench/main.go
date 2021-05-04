b140ce50e48259ca6d14624e4099f49287d20ce7
simitt <silvia.mitter@elastic.co>
2021-05-04 20:57:29 +0200

    add benchmarks

--- systemtest/cmd/apmbench/main.go
+++ systemtest/cmd/apmbench/main.go
@@ -19,33 +19,16 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
 	"testing"
 
+	"go.elastic.co/apm"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/elastic/apm-server/systemtest/benchtest"
 )
-
-func Benchmark1000Transactions(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		tracer := benchtest.NewTracer(b)
-		for pb.Next() {
-			for i := 0; i < 1000; i++ {
-				tracer.StartTransaction("name", "type").End()
-			}
-			// TODO(axw) implement a transport that enables streaming
-			// events in a way that we can block when the queue is full,
-			// without flushing. Alternatively, make this an option in
-			// TracerOptions?
-			tracer.Flush(nil)
-		}
-		stats := tracer.Stats()
-		if n := stats.Errors.SendStream; n > 0 {
-			b.Errorf("expected 0 transport errors, got %d", n)
-		}
-	})
-}
 
 func BenchmarkOTLPTraces(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
@@ -68,8 +51,138 @@ func BenchmarkOTLPTraces(b *testing.B) {
 	})
 }
 
+func Benchmark1000Transactions(b *testing.B) {
+	runBenchmark(b, 0, 0, withSpans)
+}
+
+func Benchmark1000_5_5_Spans(b *testing.B) {
+	runBenchmark(b, 5, 5, withSpans)
+}
+
+func Benchmark1000_15_15_Spans(b *testing.B) {
+	runBenchmark(b, 15, 15, withSpans)
+}
+
+func Benchmark1000_30_30_Spans(b *testing.B) {
+	runBenchmark(b, 30, 30, withSpans)
+}
+
+func Benchmark1000_50_50_Spans(b *testing.B) {
+	runBenchmark(b, 50, 50, withSpans)
+}
+
+func Benchmark1000_5_5_Errors(b *testing.B) {
+	runBenchmark(b, 5, 5, withErrors)
+}
+
+func Benchmark1000_10_10_Errors(b *testing.B) {
+	runBenchmark(b, 10, 10, withErrors)
+}
+
+func Benchmark1000_15_15_Errors(b *testing.B) {
+	runBenchmark(b, 15, 15, withErrors)
+}
+
+func Benchmark1000_30_50_Errors(b *testing.B) {
+	runBenchmark(b, 30, 50, withErrors)
+}
+
+func Benchmark1000_30_70_Errors(b *testing.B) {
+	runBenchmark(b, 30, 70, withErrors)
+}
+
+func Benchmark1000_30_80_Errors(b *testing.B) {
+	runBenchmark(b, 30, 80, withErrors)
+}
+
+func Benchmark1000_30_100_Errors(b *testing.B) {
+	runBenchmark(b, 30, 100, withErrors)
+}
+
+func runBenchmark(b *testing.B, n int, frames int, fn func(*apm.Tracer, int, int)) {
+	b.RunParallel(func(pb *testing.PB) {
+		tracer := benchtest.NewTracer(b)
+		for pb.Next() {
+			for i := 0; i < 1000; i++ {
+				fn(tracer, n, frames)
+			}
+			// TODO(axw) implement a transport that enables streaming
+			// events in a way that we can block when the queue is full,
+			// without flushing. Alternatively, make this an option in
+			// TracerOptions?
+			tracer.Flush(nil)
+		}
+		stats := tracer.Stats()
+		if n := stats.Errors.SendStream; n > 0 {
+			b.Errorf("expected 0 transport errors, got %d", n)
+		}
+	})
+
+}
+
+func withSpans(tracer *apm.Tracer, n int, stacktraces int) {
+	// set the maximum number of stacktraces
+	tracer.SetStackTraceLimit(stacktraces)
+	// create a transaction with n spans
+	tx := tracer.StartTransaction(fmt.Sprintf("with-spans-%d-stacktraces-%d", n, stacktraces), "request")
+	defer tx.End()
+	var parentSpan *apm.Span
+	for i := 0; i < n; i++ {
+		span := tx.StartSpan(fmt.Sprintf("SELECT FROM foo %d", i), "db.mysql.query", parentSpan)
+		if stacktraces > 0 {
+			spanWithStacktrace(span, stacktraces, 0)
+		}
+		defer span.End()
+	}
+	tx.Result = "HTTP 2xx"
+}
+
+func spanWithStacktrace(span *apm.Span, n int, current int) {
+	if current < n {
+		spanWithStacktrace(span, n, current+1)
+	} else {
+		span.SetStacktrace(0)
+	}
+}
+
+func withErrors(tracer *apm.Tracer, n int, stacktraces int) {
+	// set the maximum number of stacktraces
+	tracer.SetStackTraceLimit(stacktraces)
+	// create a transaction with n errors
+	tx := tracer.StartTransaction(fmt.Sprintf("with-errors-%d-stacktraces-%d", n, stacktraces), "request")
+	defer tx.End()
+	for i := 0; i < n; i++ {
+		e := tracer.NewError(errors.New("boom"))
+		if stacktraces > 0 {
+			errorWithStacktrace(e, stacktraces, 0)
+		}
+		e.SetTransaction(tx)
+		e.Send()
+	}
+	tx.Result = "HTTP 4xx"
+}
+
+func errorWithStacktrace(e *apm.Error, n int, current int) {
+	if current < n {
+		errorWithStacktrace(e, n, current+1)
+	} else {
+		e.SetStacktrace(0)
+	}
+}
+
 func main() {
 	if err := benchtest.Run(
+		Benchmark1000_30_100_Errors,
+		Benchmark1000_30_80_Errors,
+		Benchmark1000_30_70_Errors,
+		Benchmark1000_30_50_Errors,
+		Benchmark1000_15_15_Errors,
+		Benchmark1000_10_10_Errors,
+		Benchmark1000_5_5_Errors,
+		Benchmark1000_50_50_Spans,
+		Benchmark1000_30_30_Spans,
+		Benchmark1000_15_15_Spans,
+		Benchmark1000_5_5_Spans,
 		Benchmark1000Transactions,
 		BenchmarkOTLPTraces,
 	); err != nil {
